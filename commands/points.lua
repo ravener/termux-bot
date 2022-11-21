b58371ef0546144529d2fe721a89e7aa0f4dad64
Raven Ravener <ravener.anime@gmail.com>
2022-11-21 18:40:08 +0430

    fix: pronoun

--- commands/points.lua
+++ commands/points.lua
@@ -17,7 +17,8 @@ local function pointsCommand(message, args, meta)
   local nextLevel = (level + 1)^2 * 100
   local nextPoints = reformatInt(nextLevel - tonumber(points))
   local target = user == message.author and "You are" or user.username .. " is"
-  return string.format("%s currently level **%d** with **%s** bits.\nYou need **%s** bits for the next level (**%s** more to go!)", target, level, bits, reformatInt(nextLevel), nextPoints)
+  local pronoun = user == message.author and "You" or "They"
+  return string.format("%s currently level **%d** with **%s** bits.\n%s need **%s** bits for the next level (**%s** more to go!)", target, level, bits, pronoun, reformatInt(nextLevel), nextPoints)
 end
 
 return {
